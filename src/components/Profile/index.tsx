f341dbad1b01d3aa45caf36cbb6f899a3afa7093
Marcelo Barboza <marcelo.barboza.cmkp@gmail.com>
2021-06-23 11:21:14 -0300

    feito o segundo dia de aula NLW

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Avatar } from '../Avatar'
+import { styles } from './style'
+
+export function Profile() {
+  return (
+    <View style={styles.container}>
+      <Avatar urlImage="https://github.com/Marcelo7211.png" />
+
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>
+            Olá,
+          </Text>
+          <Text style={styles.username}>
+            Marcelo
+          </Text>
+        </View>
+
+        <Text style={styles.message}>
+          Hoje é dia de vitória
+        </Text>
+
+      </View>
+    </View>
+  )
+}
